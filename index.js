c865b3b22f4f855c9fcf91522208a1e6cd0f41f6
Kaighassy2002 <kaighassysuresh@gmail.com>
2024-03-25 17:26:48 +0530

    json server of book shop

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const BSServer = jsonServer.create()
+const middleware = jsonServer.defaults()
+const router = jsonServer.router('db.json')
+const PORT = 3000 || process.env.PORT
+
+BSServer.use(middleware)
+BSServer.use(router)
+
+BSServer.listen(PORT,()=>{
+    console.log(`Book shop server startednat port ${PORT}`);
+})
