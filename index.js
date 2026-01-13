9c0df280110c7d2e57284916f024c859c9678d23
Kaighassy2002 <kaighassysuresh@gmail.com>
2026-01-13 12:00:12 +0530

    v2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const jsonServer = require('json-server')
 const BSServer = jsonServer.create()
 const middleware = jsonServer.defaults()
 const router = jsonServer.router('db.json')
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 BSServer.use(middleware)
 BSServer.use(router)
 
 BSServer.listen(PORT,()=>{
-    console.log(`Book shop server startednat port ${PORT}`);
+    console.log(`Book shop server started at port ${PORT}`);
 })
